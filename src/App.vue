e8617112235c9a8afead18b5b2e82a335af7c838
tang_huibo <1215893264@qq.com>
2020-11-18 23:02:05 +0800

    升级vue3.0

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     <img src="./assets/logo.png">
     <div>
       <p>
-        If Element is successfully added to this project, you'll see an
+        If Element Plus is successfully added to this project, you'll see an
         <code v-text="'<el-button>'"></code>
         below
       </p>
-      <el-button>el-button</el-button>
+      <el-button type="primary">el-button</el-button>
     </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -17,7 +17,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld
   }
@@ -26,7 +26,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './plugins/element.js'
+import installElementPlus from './plugins/element.js'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+app.mount('#app')
